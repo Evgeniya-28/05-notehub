77a6a3a09ecba30454913d643bc8b6673184457b
Evgeniya Kosykh <evgeniyapylypenko92@gmail.com>
2026-01-20 16:00:20 +0200

    create and modified files

---
+++ src/services/movieService.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import type { Movie } from "../types/movie";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+export interface FetchMoviesResponse {
+  results: Movie[];
+  total_pages: number;
+  page: number;
+}
+
+export async function fetchMovies(
+  query: string,
+  page = 1,
+): Promise<FetchMoviesResponse> {
+  const response = await axios.get<FetchMoviesResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: { query, page },
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    },
+  );
+
+  return response.data;
+}
